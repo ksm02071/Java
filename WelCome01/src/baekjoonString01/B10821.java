c3abda3a55e5c70f6094902b2f21646a023f916a
ksm0207 <ksm03071@gmail.com>
2021-06-06 18:19:48 +0900

    Create B10821.java
    - 문제 : 10821번 정수의 개수
    - 분류 : 문자열 ,애드혹,  파싱

---
+++ WelCome01/src/baekjoonString01/B10821.java
@@ -0,0 +1,27 @@
+package baekjoonString01;
+
+import java.io.BufferedReader;
+import java.io.InputStreamReader;
+
+public class B10821 {
+	
+	public static void main(String[] args) throws Exception {
+		// Git 문제 10821번 정수의 개수 - 백준 알고리즘 분류 : 문자열 애드 혹 파싱
+		
+		// 1. 숫자와 콤마로만 이루어진 문자열 S에서 정수의 개수를 구하는 프로그램 만들기
+		
+		BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+		String [] array = null;
+		String S = br.readLine();
+		int count = 0;
+		
+		array = S.split(",");
+		
+		for (String res : array) {
+			count ++;
+		}
+		
+		System.out.print(count);
+
+	}
+}
